bb5d3c8e510b4cdcdae29fb273669a22706da490
Dev aadi <adityasaini35947@gmail.com>
2026-02-01 01:30:21 +0530

    update test.routes.j

--- backend/src/routes/test.routes.js
+++ backend/src/routes/test.routes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router.post("/create-test-user", async(req, res) => {
   try {
     const user = await User.create({
-      name: "Test User3",
-      email: "test@chatflowhghhub.com",
-      password: "123456ghfhgh",
+      name: "Test User3wew",
+      email: "test@chatflowweqewhghhub.com",
+      password: "12345qwew6ghfhgh",
     });
 
     res.json(user);

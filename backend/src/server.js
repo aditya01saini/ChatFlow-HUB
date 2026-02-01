10cc07def6f43d0c61b5c24bc4b982a4f0adc586
Dev aadi <adityasaini35947@gmail.com>
2026-02-01 22:39:19 +0530

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import testRoutes from "./routes/test.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import chatRoutes from "./routes/chat.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
 
+
 app.use(cors());
+app.use(express.json());
+app.use("/api/test", testRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.get("/", (req, res) => {
     console.log("chatflow backend is running");
